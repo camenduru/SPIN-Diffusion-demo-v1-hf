e436174677246bce90021e12768c58ac876a0561
angelahzyuan <hzyuan@ucla.edu>
2024-02-23 14:23:29 +0800

    update app.py

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
 import gradio as gr
-import cv2
 from diffusers import StableDiffusionPipeline, UNet2DConditionModel
 import torch
 import random
 import numpy as np
 
-
-MODEL="/mnt/bn/ailab-yuningshen-psg/mlx/users/quanquan.gu/playground/trl/iter0_0_2.0e-5_linear200_new/checkpoints/checkpoint_0"
-MODEL="/mnt/bn/ailab-yuningshen-psg/mlx/users/quanquan.gu/playground/trl/iter2_12h_5.0e-8_beta5_rep_wtie/checkpoints/checkpoint_10"
-PROMPTS="/mnt/bn/ailab-yuningshen-psg/mlx/users/quanquan.gu/playground/trl/dataset/pickapic/pickapic_v2/validation_unique-00007_filtered.parquet"
-NUM=1
-
-
+MODEL="UCLA-AGI/SPIN-Diffusion-iter3"
 
 def set_seed(seed=5775709):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
+
 
 set_seed()
 
@@ -42,11 +36,6 @@ def generate(prompt: str, num_images: int=5, guidance_scale=7.5):
     images = pipe(prompt, generator=generator, guidance_scale=guidance_scale, num_inference_steps=50, num_images_per_prompt=num_images).images
     images = [x.resize((512, 512)) for x in images]
     return images
-
-def gen_image(args, image):
-    output = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    return output
-
 
 
 
