150a82695b3a2ee5e5d1c099ad7fd793ee2f94ef
Angela Yuan <angelahzyuan@users.noreply.huggingface.co>
2024-02-23 12:15:13 +0000

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def generate(prompt: str, num_images: int=5, guidance_scale=7.5):
 
 with gr.Blocks() as demo:
     gr.Markdown("# SPIN-Diffusion 1.0 Demo")
-    gr.Markdown("A self-play fine-tuned diffusion model from runwayml/stable-diffusion-v1-5, using winner images from the yuvalkirstain/pickapic_v2 dataset.")
+    gr.Markdown("A **self-play** fine-tuned diffusion model from [runwayml/stable-diffusion-v1-5](https://huggingface.co/runwayml/stable-diffusion-v1-5), using winner images from the [yuvalkirstain/pickapic_v2](https://huggingface.co/datasets/yuvalkirstain/pickapic_v2) dataset.")
     with gr.Row():
         prompt_input = gr.Textbox(label="Enter your prompt", placeholder="Type something...", lines=2)
         generate_btn = gr.Button("Generate images")
@@ -42,6 +42,8 @@ with gr.Blocks() as demo:
     num_images_input = gr.Number(label="Number of images", value=5, minimum=1, maximum=10, step=1)
     gallery = gr.Gallery(label="Generated images", elem_id="gallery", columns=5, object_fit="contain")
     
+    gr.Markdown("```If a generated image appears entirely black, it has been filtered out by the NSFW safety checker. Please try generating additional images.```")
+
     # Define your example prompts
     examples = [
         ["The Eiffel Tower at sunset"],
@@ -57,6 +59,4 @@ with gr.Blocks() as demo:
     
     generate_btn.click(fn=generate, inputs=[prompt_input, num_images_input, guidance_scale], outputs=gallery)
     
-    gr.Markdown("If a generated image appears entirely black, it has been filtered out by the NSFW safety checker. Please try generating additional images.")
-
 demo.queue().launch()
